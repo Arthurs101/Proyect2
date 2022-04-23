5f3dee509c5220eb1d73cc319b61f5cbfa8f9d6e
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2022-04-23 17:57:50 -0600

    Add files via upload

--- demo/src/main/java/com/proyect2/demo/controller/form/model/RegisterForm.java
+++ demo/src/main/java/com/proyect2/demo/controller/form/model/RegisterForm.java
@@ -2,6 +2,10 @@ package com.proyect2.demo.controller.form.model;
 
 import com.service.dto.PersonDTO;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterForm {
     private PersonDTO person;
     public void setperson(PersonDTO person) {
