a0258118a46115a83e2e809e98d1e21f4715940f
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2022-04-22 19:08:27 -0600

    Add files via upload
    Changes to register

---
+++ demo/src/main/java/com/proyect2/demo/controller/form/UserController.java
@@ -0,0 +1,29 @@
+package com.proyect2.demo.controller.form;
+
+import java.util.HashMap;
+
+import com.proyect2.demo.controller.form.model.RegisterForm;
+import com.service.dto.PersonDTO;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/new")
+    public ModelAndView newUser(){//registrar nuevo usuario
+        RegisterForm form = new RegisterForm();
+        form.setperson(new PersonDTO());
+        var params = new HashMap<String, Object>();
+        params.put("form", form);
+        return new ModelAndView("register", params);
+    }
+    @PostMapping("/save")
+    public ModelAndView saveUser(){
+        return new ModelAndView ("index");
+    }
+}
